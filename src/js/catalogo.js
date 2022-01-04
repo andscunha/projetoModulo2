1ec46616d2ce250855d8297307d9964eb7521ab3
thaihc <thaisherculanodecarvalho@gmail.com>
2022-01-04 11:40:25 -0300

    refiz o estilo das páginas

---
+++ src/js/catalogo.js
@@ -0,0 +1,22 @@
+// Manipula input e chama requisição API
+
+//controller
+var botao = document.querySelector('.form__submit')
+var filme = document.getElementById('mySearch');
+var filme1 = document.getElementById('mySearch').value;
+
+botao.addEventListener('click' ,()=>{
+    enviar()
+  
+})
+
+function enviar() {
+    sessionStorage.setItem("item_pesq", filme.value);
+    location.href = "./informacoes.html";
+  }
+
+  function homeFilme(filme1) {
+    sessionStorage.setItem("item_pesq", filme1)
+    location.href = "./informacoes.html"
+  }
+
---
+++ src/js/resultado.js
@@ -0,0 +1,48 @@
+//Models
+
+var filme1 = sessionStorage.getItem("item_pesq");
+
+async function apiFilme(filme1) {
+  const chave = "b0df158e";
+  let url = `https://www.omdbapi.com/?apikey=${chave}&t=${filme1}`;
+  let respostaApi = await fetch(url).then((response) => response.json());
+  console.log(respostaApi);
+  try {
+    if (respostaApi.Title !== undefined) {
+      sucesso(respostaApi);
+    } else {
+      throw new Error("O filme não foi encontrado");
+    }
+  } catch (error) {
+    erro(error);
+  }
+}
+
+apiFilme(filme1);
+
+// View
+function sucesso(respostaApi) {
+  $("#titulo").html(`${respostaApi.Title}`);
+  $("#diretor").html(`Diretor: ${respostaApi.Director}`);
+  $("#ano").html(`Ano: ${respostaApi.Year}`);
+  $("#lancamento").html(`Lançamento: ${respostaApi.Release}`);
+  $("#duracao").html(`Duração: ${respostaApi.Runtime}`);
+  $("#genero").html(`Gênero: ${respostaApi.Genre}`);
+  $("#atores").html(`Atores: ${respostaApi.Actors}`);
+  $("#sinopse").html(`Sinopse: ${respostaApi.Plot}`);
+  $("#avaliacao-imdb").html(`Avaliação IMDB: ${respostaApi.imdbRating}`);
+  $("#poster-filme").attr("src", respostaApi.Poster);
+}
+
+function erro(error) {
+  $("#diretor").empty();
+  $("#ano").empty();
+  $("#lancamento").empty();
+  $("#duracao").empty();
+  $("#genero").empty();
+  $("#atores").empty();
+  $("#sinopse").empty();
+  $("#avaliacao-imdb").empty();
+  $("#poster-filme").empty();
+  $("#titulo").html(`${error}`);
+}
