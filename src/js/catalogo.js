2eade105c9c27405e4b1c8feb587d2c1f0a60dcb
thaihc <thaisherculanodecarvalho@gmail.com>
2022-01-04 15:30:42 -0300

    arrumei a versao mobile

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,22 +1,26 @@
 // Manipula input e chama requisição API
 
 //controller
-var botao = document.querySelector('.form__submit')
-var filme = document.getElementById('mySearch');
-var filme1 = document.getElementById('mySearch').value;
+var botao = document.querySelector(".form__submit");
+var filme = document.getElementById("mySearch");
+var filme1 = document.getElementById("mySearch").value;
 
-botao.addEventListener('click' ,()=>{
-    enviar()
-  
-})
+botao.addEventListener("click", () => {
+  enviar();
+});
 
 function enviar() {
-    sessionStorage.setItem("item_pesq", filme.value);
-    location.href = "./informacoes.html";
-  }
+  sessionStorage.setItem("item_pesq", filme.value);
+  location.href = "./informacoes.html";
+}
 
-  function homeFilme(filme1) {
-    sessionStorage.setItem("item_pesq", filme1)
-    location.href = "./informacoes.html"
-  }
+function homeFilme(filme1) {
+  sessionStorage.setItem("item_pesq", filme1);
+  location.href = "./informacoes.html";
+}
 
+$(document).keypress(function (e) {
+  if (e.which == 13) {
+    $(".form__submit").click();
+  }
+});
