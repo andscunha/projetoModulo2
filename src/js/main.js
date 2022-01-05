913ce11f434a4e5836c5cac392ed3376c16548da
thaihc <thaisherculanodecarvalho@gmail.com>
2022-01-05 17:00:48 -0300

    alterei o caminho das pastas

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,15 @@
 function validaEmail() {
-    let dx = document.getElementById("email");
-    if (dx.value.length === 0) {
-      dx.classList.remove("input-invalid");
-      return false;
-    }
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(dx.value)) {
-      sessionStorage.setItem('item', email.value)
-      window.location = "./views/cadastro/cadastro.html"
-      dx.classList.remove("input-invalid");
-      return true;
-    }
-    alert('E-mail inválido!')
-    dx.classList.add("input-invalid");
+  let dx = document.getElementById("email");
+  if (dx.value.length === 0) {
+    dx.classList.remove("input-invalid");
     return false;
   }
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(dx.value)) {
+    window.location = "./html/cadastro.html";
+    dx.classList.remove("input-invalid");
+    return true;
+  }
+  alert("E-mail inválido!");
+  dx.classList.add("input-invalid");
+  return false;
+}
